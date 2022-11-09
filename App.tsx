df1b51fa9c16cda71ecb0c407a20e7ae2696092d
LUIZ VILARINHO NETO <luiz.vilarinho@portoseguro.com.br>
2022-11-08 23:51:09 -0300

    nlw aula 3

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { NativeBaseProvider, StatusBar} from "native-base";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto";
+import {AuthContextProvider} from "./src/contexts/AuthContext";
 
+import { New } from "./src/screens/new"
+import { Loading } from './src/components/Loading';
 import { THEME } from './src/styles/theme';
-import { Loading } from './src/components/loading';
 import { Signin } from './src/screens/Signin';
+import {Pools} from "./src/screens/Pools";
 
 export default function App() {
   const [fontLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
@@ -12,13 +15,16 @@ export default function App() {
   return (
       <SafeAreaProvider>
         <NativeBaseProvider theme={ THEME }>
-        <StatusBar 
-            barStyle="light-content"
-            backgroundColor={"transparent"}
-            translucent
-          />
+            <AuthContextProvider>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor={"transparent"}
+                    translucent
+                  />
 
-        {fontLoaded? <Signin /> : <Loading /> }
+                {fontLoaded? <Pools /> : <Loading /> }
+
+            </AuthContextProvider>
 
         </NativeBaseProvider>
       </SafeAreaProvider>
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,11 +1,28 @@
-import { Center, Text } from "native-base";
+import {Center, Image, Text, Icon} from "native-base";
 import Logo from "../assets/logo.svg";
+import {Button} from "../components/button";
+import {Fontisto} from "@expo/vector-icons";
+import { useAuth } from "../hooks/useAuth"
 
 export function Signin(){
+
+    const {signIn,user} = useAuth();
+    console.log("dados do usuario", user)
+
     return(
         <Center flex={1} bgColor="gray.900">
-            <Logo />
-          <Text color={'white'} fontSize={24} fontFamily="heading">Signin</Text>
+            <Logo width={212} height={40} color={"white"}/>
+           <Button
+               type={"SECUNDARY"}
+               title={"Entrar com o google"}
+               leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+               isLoading={false}
+               mt={12}
+               onPress={signIn}
+           />
+          <Text color={'white'} textAlign={"center"} mt={4}>
+              Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação de sua conta.
+          </Text>
         </Center>
     )
 }
